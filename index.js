e6f965ba1fc7db752bb61260e6ae764a747b6178
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2022-02-04 04:57:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,41 +3,43 @@ let hexcode = [];
 let a;
 let myElement = document.querySelector(".background-all");
 
-let finalColor = "#191919" //initial color (I could have made it random too)
+let finalColor = "" //initial color (I could have made it random too)
 document.querySelector(".result").innerHTML = finalColor;
 myElement.style.backgroundColor = finalColor;
 
 let counter = 0;
 let allColors = [];
 
-
 const hexcodeFunc = () => {
-
     //generating the random letters and numbers as an array
     for (let i = 0; i<6; i++) {
         a = chars[Math.floor(Math.random()*15)]
         hexcode[i] = a;
     };
-
     let hexcodeString = hexcode.toString();
-
-    finalColor = "#"+hexcodeString.replace(/,/g,"") //deleting commas
     
-    document.querySelector(".result").innerHTML = finalColor;
+    finalColor = "#"+hexcodeString.replace(/,/g,"") //deleting commas
 
-    myElement.style.backgroundColor = finalColor; //set finalColor as the background color
+    return finalColor
+}
 
+//start screen with some text and a random background
+let firstColor = hexcodeFunc()
+myElement.style.backgroundColor = firstColor;
+document.querySelector(".result").innerHTML = "<div>Let's begin!<br><h6>(click to copy the hex code)</h6></div>";
+
+const hexcodeToPage = () => {
+    document.getElementById("previous-container").style.display = "block"
+
+    myElement.style.backgroundColor = hexcodeFunc()
 
     //putting previous colors on the screen as little boxes
     let div = document.createElement("div");
-    div.style.width = "70px";
-    div.style.height = "20px";
-    div.style.background = finalColor;
-    div.style.color = "white";
-    div.style.border = "solid black"
-    div.style.margin = "1px";
-    div.style.borderWidth = "3.5px"
-
+    div.style.width = "75px";
+    div.style.height = "30px";
+    div.style.backgroundColor = finalColor;
+    div.style.borderRadius = "10px";
+    div.style.paddingTop = "6px";
     div.innerHTML = finalColor;
 
     document.getElementById("previous").appendChild(div);
@@ -45,19 +47,15 @@ const hexcodeFunc = () => {
     //storing the previous colors
     allColors[counter] = finalColor;
     counter++;
+
+    //current color
+    document.querySelector(".result").innerHTML = allColors[counter-1];
+
+    let elem = document.getElementById('previous-container');
+    elem.scrollTop = elem.scrollHeight;
 }
 
-//a function to copy the current color when user clicks on the text under the button
+//a function to copy the current color when user clicks on the text under the main button
 const copy = (copyId) => {
-    let inputElement = document.createElement("input");
-    inputElement.type = "text";
-
-    let copyText = document.getElementById(copyId).innerHTML;
-    inputElement.value = copyText;
-
-    document.body.appendChild(inputElement);
-    inputElement.select();
-    document.execCommand('copy');
-    document.body.removeChild(inputElement);
-
+    navigator.clipboard.writeText(finalColor)
 }
